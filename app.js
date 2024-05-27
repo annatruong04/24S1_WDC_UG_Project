6fbe686457bab3aed519b3dcb6c2cdecb3bb7aef
ducnamkhanhle <ducnamkhanh.le@student.adelaide.edu.au>
2024-05-27 04:49:35 +0000

    starting login-system

--- app.js
+++ app.js
@@ -16,25 +16,24 @@ var app = express();
 app.use(cors());
 // view engine setup
 app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use(express.json()); // This line is crucial
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: true
-// }));
 
 // Create a connection object
 const connection = mysql.createConnection({

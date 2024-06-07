6d803e1729fefbde8949180ce918e43e1030b24b
ducnamkhanhle <ducnamkhanh.le@student.adelaide.edu.au>
2024-06-07 05:26:49 +0000

    add update for manager

---
+++ public/javascripts/manager_js/postUpdate.js
@@ -0,0 +1,60 @@
+var appdiv = new Vue({
+    el: "#manager-home-page",
+    data: {
+        imgErr: false,
+        form: {
+            Title: '',
+            Message: '',
+            Type: '',
+        }
+    },
+    mounted: function() {
+    },
+    methods: {
+        async submitForm() {
+            const formData = new FormData();
+
+            formData.append('Title', this.form.Title);
+            formData.append('Message', this.form.Message);
+            formData.append('Type', this.form.Type);
+
+            for (var pair of formData.entries()) {
+                console.log(pair[0]+ ': ' + pair[1]);
+            }
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", '/api/manager/create/updates', true);
+
+            xhr.onload = () => {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    window.location.href = `http://localhost:3000/manager/Update.html`;
+                    alert('Add update Successfully');
+                } else {
+                    console.error('The request failed:', xhr.statusText);
+                }
+            };
+
+            xhr.onerror = () => {
+                console.error('Network error');
+            };
+
+            xhr.send(formData);  // Send FormData instead of JSON
+        },
+    }
+});
+
+
+window.onload = function () {
+    var app = new Vue({
+      el: "#mydiv",
+      data: {
+        clicked: false
+      },
+      methods: {
+        toggleDropdown() {
+          console.log(this.clicked);
+          this.clicked = !this.clicked;
+        }
+      }
+    });
+  }
---
+++ public/javascripts/manager_js/update.js
@@ -0,0 +1,104 @@
+var appdiv = new Vue({
+    el: "#manager-page",
+    data: {
+      updates: [],
+      showPopUp: false,
+      imgErr: false,
+      form: {
+        name: '',
+        location: '',
+        date: '',
+        description: ''
+      }
+    },
+    mounted: function() {
+      this.fetch_update();
+    },
+    computed: {
+        truncatedUpdates() {
+          return this.updates.map(update => {
+            return {
+              ...update,
+              truncatedMessage: this.truncateMessage(update.Message, 19), // Adjust the number of words as needed
+            };
+          });
+        }
+    },
+    methods: {
+      fetch_update() {
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.open("GET", "/api/manager/read/updates", true);
+
+        xhttp.onreadystatechange = () => {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                var data = JSON.parse(xhttp.responseText);
+                this.updates = data;
+                console.log(this.updates);
+            }
+        };
+
+        xhttp.send();
+      },
+      gotoPostUpdate(){
+        window.location.href = `http://localhost:3000/manager/postUpdate.html`;
+      },
+      directUpdate(update) {
+        const queryParams = new URLSearchParams({
+          id: update.UpdateID,
+          // Add other update details as needed
+        }).toString();
+
+        window.location.href = `http://localhost:3000/manager/update-detail.html?${queryParams}`;
+      },
+      truncateMessage(message, wordLimit) {
+        const words = message.split(' ');
+        if (words.length > wordLimit) {
+          return words.slice(0, wordLimit).join(' ') + '...';
+        }
+        return message;
+      },
+      deleteEvent(EventID, EventName){
+        if (window.confirm(`Are you sure you want to delete ${EventName}?`)) {
+          var xhttp = new XMLHttpRequest();
+
+          xhttp.onreadystatechange = () => {
+              if (xhttp.readyState === 4 && xhttp.status === 200) {
+                  window.location.href = `http://localhost:3000/manager/Event.html`;
+                  console.log("Delete Event successfull");
+              }
+          };
+          xhttp.open("post", "/api/manager/delete/events", true);
+          xhttp.setRequestHeader("Content-type", "application/json");
+          xhttp.send(JSON.stringify({eventID: EventID}));
+      }
+    },
+    editEvent(event){
+      const queryParams = new URLSearchParams({
+        id: event.EventID,
+        name: event.Name,
+        description: event.Description,
+        date: event.Date,
+        location: event.Location,
+        // Add other event details as needed
+      }).toString();
+
+      window.location.href = `http://localhost:3000/manager/editEvent.html?${queryParams}`;
+    }
+  }
+});
+
+window.onload = function () {
+    var app = new Vue({
+      el: "#mydiv",
+      data: {
+        clicked: false
+      },
+      methods: {
+        toggleDropdown() {
+          console.log(this.clicked);
+          this.clicked = !this.clicked;
+        }
+      }
+    });
+}
